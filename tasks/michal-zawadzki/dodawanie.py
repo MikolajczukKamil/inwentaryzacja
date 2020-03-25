fd3fb8d915003accc2518792058c137d890073c2
Michal <zawadzki.micha@gmail.com>
2020-03-25 16:23:05 +0100

    Zmiana funkcji Suma

--- tasks/michal-zawadzki/dodawanie.py
+++ tasks/michal-zawadzki/dodawanie.py
@@ -1,2 +1,10 @@
+class Liczba:
+    def __init__(self, wartosc):
+        self.liczba = wartosc
+
+    def Podaj(self):
+        return self.liczba
+
+
 def Suma(a, b):
-    return a + b
+    return a.Podaj() + b.Podaj()
