b4b4251a37600fd7abb16fac369fd96933214b9e
Radosław Nagiel <47043368+RadoslawNagiel@users.noreply.github.com>
2020-03-25 18:31:15 +0100

    Zadanie 2 - dodanie funkji z podawakiem

---
+++ tasks/Radoslaw-Nagiel/dodawanie.js
@@ -0,0 +1,13 @@
+class Podawak {
+  constructor(wartosc) {
+    this.wartosc = wartosc
+  }
+
+  Podaj() {
+    return this.wartosc;
+  }
+}
+
+function Suma(a, b) {
+  return a.Podaj() + b.Podaj()
+}
