fba1a35f287e481b0f872b2e7132691ce4b05e56
Maciek <maciek1651g@gmail.com>
2020-03-26 16:08:35 +0100

    zadanie2 - ulepszenie funkcji suma

--- tasks/Maciej-Dominiak/dodawanie.js
+++ tasks/Maciej-Dominiak/dodawanie.js
@@ -1,6 +1,20 @@
 //JS
 
+class Podawak
+{
+	constructor(wartosc)
+	{
+		this.wartosc = wartosc;
+	}
+	
+	Podaj()
+	{
+		return this.wartosc;
+	}
+}
+
+
 function Suma(a,b)
 {
-	return a+b;
+	return a.Podaj()+b.Podaj();
 }
