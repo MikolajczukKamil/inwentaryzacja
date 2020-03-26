ec2341ecfcb019cf387f97d02c3c8f8ad9935f72
StrongMen120 <62567650+StrongMen120@users.noreply.github.com>
2020-03-26 22:11:01 +0100

    Poprawka Funkcji

--- tasks/Gogacz/dodawanie.js
+++ tasks/Gogacz/dodawanie.js
@@ -1,5 +1,15 @@
 // JS
 
+class Podawak {
+  constructor(wartosc) {
+    this.wartosc = wartosc
+  }
+
+  Podaj() {
+    return this.wartosc;
+  }
+}
+
 function Suma(a, b) {
-  return a + b
+  return a.Podaj() + b.Podaj()
 }
