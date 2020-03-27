ca8d29d7ad84f9a9c8b4e5379c9e43ec538e18a0
Załęski_Daniel <gaa848@gmail.com>
2020-03-28 00:03:04 +0100

    Zadanie polegajace na stworzeniu kalkulatora w Xamarin Forms

---
+++ tasks/Daniel-Zaleski/kalkulator/Kalkulator/Kalkulator/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Kalkulator
+{
+
+
+    public partial class MainPage : ContentPage
+    {
+        double firstNumber;
+        double secondNumber;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            FirstNumber.TextChanged += (o, e) => firstNumber = double.Parse(FirstNumber.Text);
+            SecondNumber.TextChanged += (o, e) => secondNumber = double.Parse(SecondNumber.Text);
+            
+            Calculate.Pressed += (o, e) =>
+            {
+                Mul.Text = "Multiplication: " + firstNumber * secondNumber;
+                if (secondNumber != 0) Div.Text = "Division: " + firstNumber / secondNumber;
+                else Div.Text = "Division: cannot divide by 0!";
+                Add.Text = "Addition: " + (firstNumber + secondNumber);
+                Sub.Text = "Subtraction: " + (firstNumber - secondNumber);                
+            };
+        }
+
+    }
+}
