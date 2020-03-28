035c1bbc3f3db343b3561383c7b9479a3c2728c5
Sebastian Marciniak <sebastian.marciniak@gmail.com>
2020-03-28 19:39:23 +0100

    Setting up space + adding test button
    Setting up again new space, now with shorter path + adding test button

---
+++ tasks/Sebastian-Marciniak/Cal/Cal/Cal/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Cal
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private int count;
+		private void Button_OnClicked(object sender, EventArgs e)
+		{
+			count++;
+			((Button) sender).Text = $"You clicked {count} times.";
+		}
+	}
+}
