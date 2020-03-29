59ca0b3cd2ba666fe699c6a45565b58560135147
Sebastian Marciniak <sebastian.marciniak@gmail.com>
2020-03-29 01:55:07 +0100

    Calculator has been done
    Calculate function has been added
    IMG to layout of calculator has been added
    Calculator is working but only when user put numbers in input boxes.

--- tasks/Sebastian-Marciniak/Cal/Cal/Cal/MainPage.xaml.cs
+++ tasks/Sebastian-Marciniak/Cal/Cal/Cal/MainPage.xaml.cs
@@ -14,11 +14,25 @@ namespace Cal
 			InitializeComponent();
 		}
 
-		private int count;
+
 		private void Button_OnClicked(object sender, EventArgs e)
 		{
-			count++;
-			((Button) sender).Text = $"You clicked {count} times.";
+			Calculate();
+		}
+
+		private void Calculate()
+		{
+			double a = Convert.ToDouble(Input_a.Text);
+			double b = Convert.ToDouble(Input_b.Text);
+			double addition = a + b;
+			double substraction = a - b;
+			double multiplication = a * b;
+			double division = a / b;
+
+			Label_Addition.Text = Convert.ToString(addition);
+			Label_Substraction.Text = Convert.ToString(substraction);
+			Label_Multiplication.Text = Convert.ToString(multiplication);
+			Label_Division.Text = Convert.ToString(division);
 		}
 	}
 }
