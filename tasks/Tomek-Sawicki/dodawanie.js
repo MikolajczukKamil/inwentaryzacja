c089ea45c01a0a0ab2c6f5d8f0c5cc9435bf2a9b
Tomek Sawicki <sawitommy@gmail.com>
2020-03-29 19:54:49 +0200

    edit dodawanie.js

--- tasks/Tomek-Sawicki/dodawanie.js
+++ tasks/Tomek-Sawicki/dodawanie.js
@@ -1,7 +1,15 @@
 // JS
 
-const tablica = [0, 1, 2, 3]
+class Podawak {
+  constructor(wartosc) {
+    this.wartosc = wartosc
+  }
+
+  Podaj() {
+    return this.wartosc;
+  }
+}
 
 function Suma(a, b) {
-  return tablica[a] + tablica[b]
+  return a.Podaj() + b.Podaj()
 }
