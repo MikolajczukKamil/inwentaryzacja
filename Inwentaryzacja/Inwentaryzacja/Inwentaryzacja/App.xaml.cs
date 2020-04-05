26cb4dde40f40ca00344d47818d14b11dee306b4
Maciek <maciek1651g@gmail.com>
2020-04-05 19:40:57 +0200

    Utworzenie struktury widoku skanowania

--- Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/App.xaml.cs
+++ Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Inwentaryzacja
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new ScanPage());
         }
 
         protected override void OnStart()
---
+++ Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/ScanPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Inwentaryzacja
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScanPage : ContentPage
+    {
+        public ScanPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Cancel(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+        private void EndScanning(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
