1cbcc729205476fc5901caf678d306b73b407ee6
Maciek <maciek1651g@gmail.com>
2020-04-10 10:56:22 +0200

    Zaimplementowanie widoku z kamery i skanowania kodów

--- Inwentaryzacja/Inwentaryzacja/Inwentaryzacja.Android/MainActivity.cs
+++ Inwentaryzacja/Inwentaryzacja/Inwentaryzacja.Android/MainActivity.cs
@@ -18,8 +18,15 @@ namespace Inwentaryzacja.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+            ZXing.Net.Mobile.Forms.Android.Platform.Init();
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            global::ZXing.Net.Mobile.Forms.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
--- Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/ScanItemPage.xaml.cs
+++ Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/ScanItemPage.xaml.cs
@@ -17,9 +17,21 @@ namespace Inwentaryzacja
             InitializeComponent();
         }
 
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _scanner.IsScanning = true;
+        }
+
+        protected override void OnDisappearing()
+        {
+            _scanner.IsScanning = false;
+            base.OnDisappearing();
+        }
+
         async void Cancel(object sender, EventArgs e)
         {
-            bool response = await DisplayAlert("Anulować?", "Czy na pewno chcesz anulować skanowanie?", "Tak", "Nie");
+            bool response = await DisplayAlert("Anulować skanowanie?", "Czy na pewno chcesz anulować skanowanie?", "Tak", "Nie");
 
             if(response)
             {
@@ -30,6 +42,14 @@ namespace Inwentaryzacja
         private void EndScanning(object sender, EventArgs e)
         {
 
+        }
+
+        private void ZXingScannerView_OnScanResult(ZXing.Result result)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await DisplayAlert("Wynik skanowania", result.Text, "OK");
+            });
         }
     }
 }
