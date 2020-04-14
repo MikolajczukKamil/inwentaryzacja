f66c0331baac2656025486f847df3ccc1fb7e47d
Jakub Ćwikowski <kcwikowski007@yahoo.com>
2020-04-14 18:07:11 +0200

    Kosmetyczne poprawki i porządki
    Uporządkowałem trochę napisany przez Ciebie kod. Myślę, że poprawiło to czytelność, a i w projekcie jest trochę większy porządek. Daj znać co sądzisz o tych zmianach.

---
+++ Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/view_models/ItemViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Inwentaryzacja.models;
+using System.Collections.Generic;
+
+namespace Inwentaryzacja.view_models
+{
+    public class ItemViewModel
+    {
+        public List<Item> ListItems { get; set; }
+        public ItemViewModel()
+        {
+            ListItems = createMockItems();
+        }
+
+        private List<Item> createMockItems()
+        {
+            List<Item> items = new List<Item>()
+            {
+                new Item ("Krzesło",12334,12,"Yes.jpg"),
+                new Item ("Krzesło",33334,3,"No.jpg"),
+                new Item ("Krzesło",111334,2,"No.jpg"),
+                new Item ("Stół",34134,41,"Yes.jpg"),
+                new Item ("Tablica",997,12,"Yes.jpg"),
+                new Item ("Stół",33334,121,"Yes.jpg"),
+                new Item ("Krzesło",12334,1,"Yes.jpg"),
+                new Item ("Stół",0700,16,"No.jpg")
+            };
+            return items;
+        }
+    }
+}
