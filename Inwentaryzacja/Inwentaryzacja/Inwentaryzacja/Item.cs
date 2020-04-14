dfa87a6948f0a46ee023037ddcff8046539d849f
Przemek <PakaGogol@gmail.com>
2020-04-14 15:18:04 +0200

    Poprawki
    Poprawkone nazwy
    dodana klasa item

---
+++ Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/Item.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inwentaryzacja
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int ID_items { get; set; }
+        public int Room { get; set; }
+        public string UrlPicture { get; set; }
+
+        public List<Item> GetItem()
+        {
+            List<Item> items = new List<Item>()
+            {
+                new Item(){Name="Krzesło",ID_items=12334,Room=12,UrlPicture="Yes.jpg"},
+                new Item(){Name="Krzesło",ID_items=33334,Room=3,UrlPicture="No.jpg"},
+                new Item(){Name="Krzesło",ID_items=111334,Room=2,UrlPicture="No.jpg"},
+                new Item(){Name="Stół",ID_items=34134,Room=41,UrlPicture="Yes.jpg"},
+                new Item(){Name="Tablica",ID_items=997,Room=12,UrlPicture="Yes.jpg"},
+                new Item(){Name="Stół",ID_items=33334,Room=121,UrlPicture="Yes.jpg"},
+                new Item(){Name="Krzesło",ID_items=12334,Room=1,UrlPicture="Yes.jpg"},
+                new Item(){Name="Stół",ID_items=0700,Room=16,UrlPicture="No.jpg"}
+            };
+            return items;
+
+        }
+        public string NewItem
+        {
+
+            get
+            {
+                return string.Format("{0} (id:{1}", Name, ID_items);
+            }
+        }
+    }
+    public class ItemViewModel
+    {
+        public List<Item> ListItems { get; set; }
+        public ItemViewModel()
+        {
+            ListItems = new Item().GetItem();
+        }
+    }
+}
--- Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/views/view-raport/RaportPage.xaml.cs
+++ Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/views/view-raport/RaportPage.xaml.cs
@@ -16,53 +16,12 @@ namespace Inwentaryzacja
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RaportPage : ContentPage
     {
-        ItemsViewModel it;
+        ItemViewModel it;
         public RaportPage()
         {
             InitializeComponent();
-            it = new ItemsViewModel();
-            Listap.ItemsSource = it.Items;
-        }
-    }
-    
-    public class Items
-    {
-        public string Nazwa { get; set; }
-        public int ID_items { get; set; }
-        public int Sale { get; set; }
-        public string urlobrazka { get; set; }
-
-        public List<Items> GetItems()
-        {
-            List<Items> itemki = new List<Items>()
-            {
-                new Items(){Nazwa="Krzesło",ID_items=12334,Sale=12,urlobrazka="Yes.jpg"},
-                new Items(){Nazwa="Krzesło",ID_items=33334,Sale=3,urlobrazka="No.jpg"},
-                new Items(){Nazwa="Krzesło",ID_items=111334,Sale=2,urlobrazka="No.jpg"},
-                new Items(){Nazwa="Stół",ID_items=34134,Sale=41,urlobrazka="Yes.jpg"},
-                new Items(){Nazwa="Tablica",ID_items=997,Sale=12,urlobrazka="Yes.jpg"},
-                new Items(){Nazwa="Stół",ID_items=33334,Sale=121,urlobrazka="Yes.jpg"},
-                new Items(){Nazwa="Krzesło",ID_items=12334,Sale=1,urlobrazka="Yes.jpg"},
-                new Items(){Nazwa="Stół",ID_items=0700,Sale=16,urlobrazka="No.jpg"}
-            };
-            return itemki;
-
-        }
-        public string NewItem
-        {
-
-            get
-            {
-                return string.Format("{0} (id:{1}", Nazwa, ID_items);
-            }
-        }
-    }
-    public class ItemsViewModel
-    {
-        public List<Items> Items { get; set; }
-        public ItemsViewModel()
-        {
-            Items = new Items().GetItems();
+            it = new ItemViewModel();
+            ItemsList1.ItemsSource = it.ListItems;
         }
     }
 }
