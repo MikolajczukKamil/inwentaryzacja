a4050e005f3cf5e447411cbad781b6382317b830
Jakub Ćwikowski <kcwikowski007@yahoo.com>
2020-04-22 12:03:06 +0200

    Przywrócenie widoku logowania
    Jak w tytule.

--- Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/App.xaml.cs
+++ Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Inwentaryzacja
         {
             InitializeComponent();
 
-            MainPage = new WelcomeViewPage();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
