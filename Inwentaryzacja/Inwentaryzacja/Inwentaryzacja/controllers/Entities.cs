1b0bfca9059d29ec26d5ec5f27d036b4d0d90247
Maciek <maciek1651g@gmail.com>
2020-05-01 21:08:29 +0200

    Dodano klasy encji plus zwracanie encji

---
+++ Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/controllers/Entities.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inwentaryzacja.controllers
+{
+    class AssetEntity
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int asset_type { get; set; }
+    }
+    class AssetTypeEntity
+    {
+        public int id { get; set; }
+        public string letter { get; set; }
+        public string name { get; set; }
+    }
+    class BuildingEntity
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+    class ReportEntity
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int room { get; set; }
+        public DateTime create_date { get; set; }
+        public int owner { get; set; }
+    }
+    class ReportAssetEntity
+    {
+        public int report_id { get; set; }
+        public int asset_id { get; set; }
+        public int? previous_room { get; set; }
+    }
+    class RoomEntity
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int building { get; set; }
+    }
+}
