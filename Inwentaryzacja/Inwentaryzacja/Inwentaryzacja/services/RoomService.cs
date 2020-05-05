557e68e99d5b95f950e0163208155fce4f1de249
lake <czerwonykubeczek@gmail.com>
2020-05-05 21:56:44 +0200

    Implementacja metod w RoomService

--- Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/services/RoomService.cs
+++ Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/services/RoomService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Inwentaryzacja.controllers;
 using Inwentaryzacja.models;
 
@@ -6,19 +7,34 @@ public class RoomService {
 	private APIController api;
 
 	public Room GetRoomById(ref int id) {
-		throw new System.Exception("Not implemented");
+
+		RoomEntity roomentity = api.GetRoomByID(id).Result;
+		Room room = new Room(roomentity.name, roomentity.building, roomentity.id);
+		return room;
 	}
 	public Room[] GetAllRooms() {
-		throw new System.Exception("Not implemented");
+
+		List<RoomEntity> roomlist = new List<RoomEntity>();
+		roomlist = api.getAllRooms().Result;
+		Room[] roomtab = new Room[roomlist.Count];
+		for (int i = 0; i < roomlist.Count; i++)
+		{
+			RoomEntity roomentity = roomlist[i];
+			Room room = new Room(roomentity.name, roomentity.building, roomentity.id);
+			roomtab[i] = room;
+		}
+		return roomtab;
 	}
 	public bool AddNewRoom(ref RoomPropotype newRoom) {
-		throw new System.Exception("Not implemented");
+		bool addnew = api.createRoom(newRoom).Result;
+		return addnew;
 	}
 	public bool DeleteRoom(ref int id) {
-		throw new System.Exception("Not implemented");
+		bool delated = api.deleteRoomByID(id).Result;
+		return delated;
 	}
 
-	private APIController aPIController;
+
 
 	private Room room;
 
