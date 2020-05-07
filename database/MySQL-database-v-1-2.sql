fafbc37738d5a1087622f01defe5b2012636aa68
Kamil Mikołajczuk <k.mikolajczuk123@gmail.com>
2020-05-07 18:21:53 +0200

    user i tabelki

--- database/MySQL-database-v-1-2.sql
+++ database/MySQL-database-v-1-2.sql
@@ -6,83 +6,95 @@
 
 /* Tables */
 
-  DROP TABLE IF EXISTS asset_types;
-
-  DROP TABLE IF EXISTS rooms;
-
-  DROP TABLE IF EXISTS assets;
-
-  DROP TABLE IF EXISTS reports;
-
+  DROP TABLE IF EXISTS login_sessions;
   DROP TABLE IF EXISTS reports_assets;
-
+  DROP TABLE IF EXISTS reports;
+  DROP TABLE IF EXISTS assets;
+  DROP TABLE IF EXISTS rooms;
+  DROP TABLE IF EXISTS buildings;
+  DROP TABLE IF EXISTS asset_types;
   DROP TABLE IF EXISTS users;
 
-  DROP TABLE IF EXISTS buildings;
+  CREATE TABLE users (
+    id TINYINT NOT NULL AUTO_INCREMENT,
+    login VARCHAR(20) NOT NULL UNIQUE,
+    hash VARCHAR(64) NOT NULL,
+    PRIMARY KEY(id)
+  );
 
-  DROP TABLE IF EXISTS login_sessions;
-
-  CREATE TABLE asset_types(
+  CREATE TABLE asset_types (
     id TINYINT NOT NULL AUTO_INCREMENT,
     letter CHAR(1) NOT NULL UNIQUE,
     name VARCHAR(20) NOT NULL UNIQUE,
     PRIMARY KEY(id)
   );
 
-  CREATE TABLE buildings(
+  CREATE TABLE buildings (
     id TINYINT NOT NULL AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL UNIQUE,
     PRIMARY KEY(id)
   );
 
-  CREATE TABLE rooms(
+  CREATE TABLE rooms (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     building TINYINT NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY(building) REFERENCES buildings(id)
+    FOREIGN KEY(building)
+      REFERENCES buildings(id)
+      ON DELETE CASCADE
   );
 
-  CREATE TABLE assets(
+  CREATE TABLE assets (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    asset_type TINYINT NOT NULL REFERENCES asset_types(id),
-    PRIMARY KEY(id)
-  );
-
-  CREATE TABLE users(
-    id TINYINT NOT NULL AUTO_INCREMENT,
-    login VARCHAR(20) NOT NULL UNIQUE,
-    salt VARCHAR(30) NOT NULL,
-    hash varchar(64) NOT NULL,
-    PRIMARY KEY(id)
+    type TINYINT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(type)
+      REFERENCES asset_types(id)
+      ON DELETE CASCADE
   );
 
   CREATE TABLE reports (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(50),
-    room INT NOT NULL REFERENCES rooms(id) ON DELETE CASCADE,
+    room INT NOT NULL,
     create_date DATETIME NOT NULL,
-    owner TINYINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    PRIMARY KEY(id)
+    owner TINYINT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(room)
+      REFERENCES rooms(id)
+      ON DELETE CASCADE,
+    FOREIGN KEY(owner)
+      REFERENCES users(id)
+      ON DELETE CASCADE
   );
 
-  /* Joining table between assets and reports */
   CREATE TABLE reports_assets (
-    report_id INT NOT NULL REFERENCES reports(id) ON DELETE CASCADE,
-    asset_id INT NOT NULL REFERENCES assets(id) ON DELETE CASCADE,
-    previous_room INT REFERENCES rooms(id) ON DELETE CASCADE,
+    report_id INT NOT NULL,
+    asset_id INT NOT NULL,
+    previous_room INT,
     present BOOLEAN NOT NULL DEFAULT TRUE,
-    PRIMARY KEY(report_id, asset_id)
+    PRIMARY KEY(report_id, asset_id),
+    FOREIGN KEY(report_id)
+      REFERENCES reports(id)
+      ON DELETE CASCADE,
+    FOREIGN KEY(asset_id)
+      REFERENCES assets(id)
+      ON DELETE CASCADE,
+    FOREIGN KEY(previous_room)
+      REFERENCES rooms(id)
   );
 
   CREATE TABLE login_sessions (
     id INT NOT NULL AUTO_INCREMENT,
-    user_id TINYINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    token varchar(64) NOT NULL,
+    user_id TINYINT NOT NULL,
+    token VARCHAR(64) NOT NULL UNIQUE,
     expiration_date DATETIME NOT NULL,
     create_date DATETIME NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id)
+      REFERENCES users(id)
+      ON DELETE CASCADE
   );
 
 /* Functions */
@@ -156,14 +168,14 @@
     BEGIN
       SELECT
         reports_assets.asset_id, reports_assets.previous_room, reports_assets.present,
-        assets.name AS asset_name, assets.asset_type,
+        assets.type AS asset_type,
         asset_types.name AS asset_type_name
       FROM
         reports_assets
       JOIN
         assets ON reports_assets.asset_id = assets.id
       JOIN
-        asset_types ON assets.asset_type = asset_types.id
+        asset_types ON assets.type = asset_types.id
       WHERE
         reports_assets.report_id = ReportId
       ;
@@ -176,7 +188,7 @@
     CREATE PROCEDURE getAssetsInRoom(IN RoomId INT)
     BEGIN
       SELECT
-        assets.id, assets.name, assets.asset_type,
+        assets.id, assets.type,
         asset_types.name AS asset_type_name,
         IFNULL(reports_assets.previous_room, 0) = 0 AS new_asset,
         IFNULL(reports_assets.previous_room, reports.room) != reports.room AS moved,
@@ -189,7 +201,7 @@
       JOIN
         assets ON reports_assets.asset_id = assets.id
       JOIN
-        asset_types ON assets.asset_type = asset_types.id
+        asset_types ON assets.type = asset_types.id
       LEFT JOIN
         rooms ON reports_assets.previous_room = rooms.id
       WHERE
@@ -210,9 +222,72 @@
       ;
     END $$ DELIMITER ;
 
+  /* Pobranie danych uzytkownika */
+
+    DROP PROCEDURE IF EXISTS getUser;
+
+    DELIMITER $$
+    CREATE PROCEDURE getUser(IN UserId INT)
+    BEGIN
+      SELECT
+        users.id, users.login, users.hash
+      FROM
+        users
+      WHERE
+        users.id = UserId
+      ;
+    END $$ DELIMITER ;
+
+  /* Pobranie sesji logowania */
+
+    DROP PROCEDURE IF EXISTS getLoginSession;
+
+    DELIMITER $$
+    CREATE PROCEDURE getLoginSession(IN user_token VARCHAR(64))
+    BEGIN
+      SELECT
+        login_sessions.id, login_sessions.user_id, login_sessions.expiration_date, login_sessions.token,
+        login_sessions.expiration_date <= NOW() AS expired
+      FROM
+        login_sessions
+      WHERE
+        login_sessions.token = user_token
+      ;
+    END $$ DELIMITER ;
+  
+  /* Utworzenie sesji logowania */
+
+    DROP PROCEDURE IF EXISTS addLoginSession;
+
+    DELIMITER $$
+    CREATE PROCEDURE addLoginSession(IN user_id INT, INT expiration_date DATETIME, IN user_token VARCHAR(64))
+    BEGIN
+      INSERT INTO
+        login_sessions (user_id, token, expiration_date, create_date)
+      VALUES 
+        (user_id, user_token, expiration_date, NOW())
+      ;
+    END $$ DELIMITER ;
+
+    /* Usunięcie sesji logowania */
+
+    DROP PROCEDURE IF EXISTS deleteLoginSession;
+
+    DELIMITER $$
+    CREATE PROCEDURE deleteLoginSession(IN user_token VARCHAR(64))
+    BEGIN
+      DELETE
+      FROM
+        login_sessions
+      WHERE 
+        login_sessions.token = user_token
+      ;
+    END $$ DELIMITER ;
+
 /* Fake data */
 
-  INSERT INTO asset_types (letter, name)
+  INSERT INTO
+    asset_types (letter, name)
   VALUES
     ('c', 'komputer'),
     ('k', 'krzesło'),
@@ -223,7 +298,8 @@
   ;
 
   /* b 34 - 1 */
-  INSERT INTO buildings (name)
+  INSERT INTO
+    buildings (name)
   VALUES
     ('b 34'),
     ('b 4'),
@@ -238,7 +314,8 @@
   ;
 
   /* from b 34 1-3 */
-  INSERT INTO rooms (name, building)
+  INSERT INTO
+    rooms (name, building)
   VALUES
     ('3/6', 1),
     ('3/40', 1),
@@ -253,211 +330,35 @@
     ('1/9', 9)
   ;
   
-  INSERT INTO assets (name, asset_type) /* 30 */
+  /* 10 * 6 */
+  INSERT INTO
+    assets (type)
   VALUES
-    ('c1', 1),
-    ('kr1', 2),
-    ('m1', 3),
-    ('p2', 4),
-    ('s1', 5),
-    ('t1', 6),
-    ('c2', 1),
-    ('kr2', 2),
-    ('m2', 3),
-    ('p2', 4),
-    ('s2', 5),
-    ('t2', 6),
-    ('c3', 1),
-    ('kr3', 2),
-    ('m3', 3),
-    ('p3', 4),
-    ('s3', 5),
-    ('t3', 6),
-    ('c4', 1),
-    ('kr4', 2),
-    ('m4', 3),
-    ('p4', 4),
-    ('s4', 5),
-    ('t4', 6),
-    ('c5', 1),
-    ('kr5', 2),
-    ('m5', 3),
-    ('p5', 4),
-    ('s5', 5),
-    ('t5', 6),
-    ('c6', 1),
-    ('kr6', 2),
-    ('m6', 3),
-    ('p6', 4),
-    ('s6', 5),
-    ('t6', 6),
-    ('c7', 1),
-    ('kr7', 2),
-    ('m7', 3),
-    ('p7', 4),
-    ('s7', 5),
-    ('t7', 6),
-    ('c8', 1),
-    ('kr8', 2),
-    ('m8', 3),
-    ('p8', 4),
-    ('s8', 5),
-    ('t8', 6),
-    ('c9', 1),
-    ('kr9', 2),
-    ('m9', 3),
-    ('p9', 4),
-    ('s9', 5),
-    ('t9', 6),
-    ('c10', 1),
-    ('k10', 2),
-    ('m10', 3),
-    ('p10', 4),
-    ('s10', 5),
-    ('t10', 6),
-    ('c11', 1),
-    ('kr11', 2),
-    ('m11', 3),
-    ('p11', 4),
-    ('s11', 5),
-    ('t11', 6),
-    ('c12', 1),
-    ('kr12', 2),
-    ('m12', 3),
-    ('p12', 4),
-    ('s12', 5),
-    ('t12', 6),
-    ('c13', 1),
-    ('kr13', 2),
-    ('m13', 3),
-    ('p13', 4),
-    ('s13', 5),
-    ('t13', 6),
-    ('c14', 1),
-    ('kr14', 2),
-    ('m14', 3),
-    ('p14', 4),
-    ('s14', 5),
-    ('t14', 6),
-    ('c15', 1),
-    ('kr15', 2),
-    ('m15', 3),
-    ('p15', 4),
-    ('s15', 5),
-    ('t15', 6),
-    ('c16', 1),
-    ('kr16', 2),
-    ('m16', 3),
-    ('p16', 4),
-    ('s16', 5),
-    ('t16', 6),
-    ('c17', 1),
-    ('kr17', 2),
-    ('m17', 3),
-    ('p17', 4),
-    ('s17', 5),
-    ('t17', 6),
-    ('c18', 1),
-    ('kr18', 2),
-    ('m18', 3),
-    ('p18', 4),
-    ('s18', 5),
-    ('t18', 6),
-    ('c19', 1),
-    ('kr19', 2),
-    ('m19', 3),
-    ('p19', 4),
-    ('s19', 5),
-    ('t19', 6),
-    ('c20', 1),
-    ('kr20', 2),
-    ('m20', 3),
-    ('p20', 4),
-    ('s20', 5),
-    ('t20', 6),
-    ('c21', 1),
-    ('kr21', 2),
-    ('m21', 3),
-    ('p21', 4),
-    ('s21', 5),
-    ('t21', 6),
-    ('c22', 1),
-    ('kr22', 2),
-    ('m22', 3),
-    ('p22', 4),
-    ('s22', 5),
-    ('t22', 6),
-    ('c23', 1),
-    ('kr23', 2),
-    ('m23', 3),
-    ('p23', 4),
-    ('s23', 5),
-    ('t23', 6),
-    ('c24', 1),
-    ('kr24', 2),
-    ('m24', 3),
-    ('p24', 4),
-    ('s24', 5),
-    ('t24', 6),
-    ('c25', 1),
-    ('kr25', 2),
-    ('m25', 3),
-    ('p25', 4),
-    ('s25', 5),
-    ('t25', 6),
-    ('c26', 1),
-    ('kr26', 2),
-    ('m26', 3),
-    ('p26', 4),
-    ('s26', 5),
-    ('t26', 6),
-    ('c27', 1),
-    ('kr27', 2),
-    ('m27', 3),
-    ('p27', 4),
-    ('s27', 5),
-    ('t27', 6),
-    ('c28', 1),
-    ('kr28', 2),
-    ('m28', 3),
-    ('p28', 4),
-    ('s28', 5),
-    ('t28', 6),
-    ('c29', 1),
-    ('kr29', 2),
-    ('m29', 3),
-    ('p29', 4),
-    ('s29', 5),
-    ('t29', 6),
-    ('c30', 1),
-    ('kr30', 2),
-    ('m30', 3),
-    ('p30', 4),
-    ('s30', 5),
-    ('t30', 6)
+    (1), (2), (3), (4), (5), (6),
+    (1), (2), (3), (4), (5), (6),
+    (1), (2), (3), (4), (5), (6),
+    (1), (2), (3), (4), (5), (6),
+    (1), (2), (3), (4), (5), (6),
+    (1), (2), (3), (4), (5), (6),
+    (1), (2), (3), (4), (5), (6),
+    (1), (2), (3), (4), (5), (6),
+    (1), (2), (3), (4), (5), (6),
+    (1), (2), (3), (4), (5), (6)
   ;
   
   /* User1 password 111 ... */
-  INSERT INTO users (login, salt, hash)
+  INSERT INTO
+    users (login, hash)
   VALUES
-    (
-      'user1', '', '$2y$10$GefRLqURuebo9eyOrYm83O0zFeGlvVt3UujXgDd02gyjZwH6NO6N6'
-    ),
-    (
-      'user2', '', '$2y$10$Ft84wWFy.ugVLQ6Fy.ObT..6xVMAje.pD5.zPYZop8pADmpWmmpDu'
-    ),
-    (
-      'user3', '', '$2y$10$rqC2MTTutu/gIrhDNUf9v.y58sclDNuWPZwLKVtIKxkJ8gfHF5P.y'
-    ),
-    (
-      'user4', '', '$2y$10$i5O.B5ZGF9lvPS5ALIXim.4dtydW4D0qjymW5e86Jv3ulJG8CRFOO'
-    ),
-    (
-      'user5', '', '$2y$10$ZM4NpuGcj.Wb0EAqRpA6JuFNw.oxJCExZdcR3uEiDEp7wYwBxJVEm'
-    )
+    ('user1', '$2y$10$GefRLqURuebo9eyOrYm83O0zFeGlvVt3UujXgDd02gyjZwH6NO6N6'),
+    ('user2', '$2y$10$Ft84wWFy.ugVLQ6Fy.ObT..6xVMAje.pD5.zPYZop8pADmpWmmpDu'),
+    ('user3', '$2y$10$rqC2MTTutu/gIrhDNUf9v.y58sclDNuWPZwLKVtIKxkJ8gfHF5P.y'),
+    ('user4', '$2y$10$i5O.B5ZGF9lvPS5ALIXim.4dtydW4D0qjymW5e86Jv3ulJG8CRFOO'),
+    ('user5', '$2y$10$ZM4NpuGcj.Wb0EAqRpA6JuFNw.oxJCExZdcR3uEiDEp7wYwBxJVEm')
   ;
   
-  INSERT INTO reports (name, room, create_date, owner)
+  INSERT INTO
+    reports (name, room, create_date, owner)
   VALUES
     ('Raport 1', 1, NOW() - INTERVAL 10 DAY, 1), /* new Room 21 report 1 */
 
@@ -473,7 +374,8 @@
   ;
 
   /* asset_id - 30 zestawów 6 elementowych */
-  INSERT INTO reports_assets (report_id, asset_id, previous_room, present)
+  INSERT INTO
+    reports_assets (report_id, asset_id, previous_room, present)
   VALUES
     /* Raport 1 */
     (1, 0 + 1, NULL, TRUE),
@@ -526,4 +428,10 @@
     (6, 18 + 4, NULL, TRUE),
     (6, 18 + 5, NULL, TRUE),
     (6, 18 + 6, NULL, TRUE)
+  ;
+
+  INSERT INTO
+    login_sessions (user_id, token, expiration_date, create_date)
+  VALUES
+    (1, 'fake-token', NOW() + INTERVAL 1 DAY, NOW() - INTERVAL 1 DAY)
   ;
