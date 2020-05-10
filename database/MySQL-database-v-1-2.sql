7ad1d73e88e9b84b5a2ab81f3b1f517d83436a05
Kamil Mikołajczuk <k.mikolajczuk123@gmail.com>
2020-05-10 14:48:04 +0200

    Bug assets

--- database/MySQL-database-v-1-2.sql
+++ database/MySQL-database-v-1-2.sql
@@ -258,7 +258,7 @@
       DECLARE Asset_room_id INT DEFAULT getRoomIdWithAsset(asset_id);
 
       SELECT
-        assets.id, assets.type, asset.letter,
+        assets.id, assets.type, assets.letter,
         asset_types.name AS asset_type_name,
         Asset_room_id AS room_id,
         rooms.name AS room_name,
