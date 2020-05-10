5a923a899ef8247fa353feac070ef3175b4091f9
Kamil Mikołajczuk <k.mikolajczuk123@gmail.com>
2020-05-10 14:49:10 +0200

    asset_types letter

--- database/MySQL-database-v-1-2.sql
+++ database/MySQL-database-v-1-2.sql
@@ -258,7 +258,7 @@
       DECLARE Asset_room_id INT DEFAULT getRoomIdWithAsset(asset_id);
 
       SELECT
-        assets.id, assets.type, assets.letter,
+        assets.id, assets.type, asset_types.letter,
         asset_types.name AS asset_type_name,
         Asset_room_id AS room_id,
         rooms.name AS room_name,
