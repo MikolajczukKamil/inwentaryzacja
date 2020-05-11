14a3d996033d23335b2635f1352256795dc058c5
Kamil M <k.mikolajczuk123@gmail.com>
2020-05-11 12:08:34 +0200

    url and label

--- Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/models/Report/ReportPositon.cs
+++ Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/models/Report/ReportPositon.cs
@@ -9,19 +9,16 @@ namespace Inwentaryzacja.models
         public Asset Asset { get; private set; }
         public bool Present { get; private set; }
         public Room PreviusRoom { get; private set; }
-        public string PictureUrl { get; private set; }
-        public string ItemData
+        public string Label
         {
             get { return $"{Asset.Name} (id:{Asset.Id})"; }
         }
 
-        public ReportPosition(Asset asset, Room previusRoom, bool present, string pictureUrl)
+        public ReportPosition(Asset asset, Room previusRoom, bool present)
         {
             Asset = asset;
             Present = present;
             PreviusRoom = previusRoom;
-            PictureUrl = pictureUrl;
         }
-
     }
 }
