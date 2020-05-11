303ac9fc8a31e114261e8975fe9010c3c41cf27c
Kamil M <k.mikolajczuk123@gmail.com>
2020-05-11 13:39:57 +0200

    Edit Entities

--- Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/controllers/Entities.cs
+++ Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/controllers/Entities.cs
@@ -5,11 +5,11 @@ using System.Text;
 
 namespace Inwentaryzacja.controllers
 {
-    public class AssetEntity
+    public class AssetInfoEntity
     {
         public int id;
-        public string name;
-        public int assetType;
+        public AssetTypeEntity type;
+        public RoomEntity room;
     }
 
     public class AssetTypeEntity
@@ -19,42 +19,40 @@ namespace Inwentaryzacja.controllers
         public string name;
     }
 
+    public class RoomEntity
+    {
+        public int id;
+        public string name;
+        public RoomEntity building;
+    }
+
     public class BuildingEntity
     {
         public int id;
         public string name;
     }
 
-    public class ReportEntity
+    public class ReportHeaderEntity
     {
         public int id;
         public string name;
-        public int room;
         public DateTime create_date;
-        public int owner;
+        public int owner_id;
+        public string owner_name;
+        public string room_name;
+        public string building_name;
     }
 
-    public class ReportWithAssetEntity
+    public class ReportPositionEntity
     {
         public int id;
-        public string name;
-        public int room;
-        public DateTime create_date;
-        public int owner;
-        public ReportAssetsEntity[] assets;
-
-        public class ReportAssetsEntity
-        {
-            public int report_id;
-            public int asset_id;
-            public int previous_room;
-        }
-    }
-
-    public class RoomEntity
-    {
-        public int id;
-        public string name;
-        public int building;
+        public int type;
+        public string asset_type_name;
+        public bool new_asset;
+        public bool moved;
+        public int moved_from_id;
+        public string moved_from_name;
+        public int previous_room;
+        public bool present;
     }
 }
