9040c8f64b970a5bc923a1ff4a73f3baa26fcd97
Przemek <PakaGogol@gmail.com>
2020-05-24 18:15:30 +0200

    Widok Starych Raportów
    Zamiana Scroll View na List View
    Dodanie Przycisków obsługujących strone. Wyświetlanie Raportów pobranych z Bazy,

--- Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/views/view-allReports/AllReportsPage.xaml.cs
+++ Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/views/view-allReports/AllReportsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Inwentaryzacja.Controllers.Api;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,34 @@ namespace Inwentaryzacja
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AllReportsPage : ContentPage
     {
+        APIController api;
         public AllReportsPage()
         {
             InitializeComponent();
+        }
+        private async void back_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new WelcomeViewPage());
+        }
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            api = new APIController();
+            List<AllReport> rp = new List<AllReport>();
+            Task<ReportHeaderEntity[]> header_task = api.getReportHeaders();
+            await header_task;
+            ReportHeaderEntity[] rep = header_task.Result;
+            for (int i = 0; i < rep.Length; i++)
+            {
+                rp.Add(new AllReport() { ReportName = rep[i].name, ReportRoom = rep[i].room.name, ReportDate = Convert.ToString(rep[i].create_date) });
+            }
+            ReportList.ItemsSource = rp;
+        }
+        public class AllReport
+        {
+            public string ReportName { get; set; }
+            public string ReportRoom { get; set; }
+            public string ReportDate { get; set; }
         }
     }
 }
