b78a4fc3b3f116b0fabfea6e5cf1cf5401c63fac
martamazurkiewicz <marta@mazurkiewi.cz>
2020-05-28 11:10:18 +0200

    Added test for ApiController.getAssetInfo and changed references

---
+++ Inwentaryzacja/UnitTests/ControllerTests/APITests/ApiControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Inwentaryzacja.Controllers.Api;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.ApiTests
+{
+    [TestFixture]
+    class ApiControllerTests
+    {
+        private APIController apiController;
+        [SetUp]
+        public void Setup()
+        {
+            apiController = new APIController();
+        }
+        [Test]
+        public async Task getAssetInfoTest()
+        {
+            AssetInfoEntity assetInfoEntity = await apiController.getAssetInfo(1);
+            Assert.AreEqual(1,assetInfoEntity.room.id);
+        }
+    }
+}
