13c81d4f76f41e3f2e4fc255932e2676be696895
lake <czerwonykubeczek@gmail.com>
2020-05-29 00:04:58 +0200

    poprawki do widoku tworzenia sali

--- Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/views/view-chooseRoom/AddRoom.xaml.cs
+++ Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/views/view-chooseRoom/AddRoom.xaml.cs
@@ -2,6 +2,7 @@
 using Inwentaryzacja.Models;
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,34 +11,48 @@ namespace Inwentaryzacja.views.view_chooseRoom
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddRoom : ContentPage
     {
+        
+        BuildingEntity[] buildings;
+        bool addedNewBuilding = false;
         APIController api = new APIController();
         
-        protected async override void OnAppearing()
-        {
-            
-            base.OnAppearing();
-            List<B> buildings_list = new List<B>();
-
-            BuildingEntity[] build = await api.getBuildings();
-
-            for (int i = 0; i < build.Length; i++)
-            {
-                buildings_list.Add(new B() {BuildingName= build[i].name });
-            }
-
-            Building_List.ItemsSource = buildings_list;
-        }
-
-        public class B
-        {
-            public string BuildingName { get; set; }
-        }
 
         public AddRoom()
         {
             InitializeComponent();
+            api.ErrorEventHandler += onApiError;
+            BindingContext = this;
+            GetBuildings();
         }
+        private async void GetBuildings()
+        {
+            Task<BuildingEntity[]> buildingTask = api.getBuildings();
+            EnableView(false);
+            await buildingTask;
+            EnableView(true);
+            buildings = buildingTask.Result;
 
+            if (buildings == null) return;
+
+            foreach (BuildingEntity item in buildings)
+            {
+                BuildingPicker.Items.Add(item.name);
+            }
+
+            if (BuildingPicker.Items.Count > 0)
+            {
+                if (addedNewBuilding)
+                {
+                    BuildingPicker.SelectedItem = BuildingPicker.Items[BuildingPicker.Items.Count - 1];
+                }
+                else
+                {
+                    BuildingPicker.SelectedItem = BuildingPicker.Items[0];
+                }
+
+            }
+        }
+  
         public void return_ChooseRoom(object o, EventArgs args)
         {
             Application.Current.MainPage = new NavigationPage(new ChooseRoomPage());
@@ -46,18 +61,17 @@ namespace Inwentaryzacja.views.view_chooseRoom
         public async void Check_Room(object o, EventArgs args)
         {
             string number = room_number.Text;
-            B budynek = (B) Building_List.SelectedItem;
-            BuildingEntity[] buildings = await api.getBuildings();
+            BuildingEntity[] allbuildings = await api.getBuildings();
             BuildingEntity mybuilding = new BuildingEntity();
-
-            foreach (var item in buildings)
+            string choosenBuildingName = BuildingPicker.Items[BuildingPicker.SelectedIndex];
+            foreach (var item in allbuildings)
             {
-                if (item.name == budynek.BuildingName)
+                if (item.name == choosenBuildingName)
                 {
                     mybuilding = item;
                 }
             }
-           
+
             RoomEntity[] rooms = await api.getRooms(mybuilding.id);
             bool roomexist = false;
 
@@ -72,18 +86,32 @@ namespace Inwentaryzacja.views.view_chooseRoom
 
             if (!roomexist)
             {
-                RoomPropotype roomprop = new RoomPropotype(number, mybuilding);
-                bool isAdded = await api.createRoom(roomprop);
+                Task<bool> createTask = api.createRoom(new RoomPropotype(number, mybuilding));
+                EnableView(false);
+                await createTask;
+                EnableView(true);
+                bool isCreated = createTask.Result;
+               
+                App.Current.MainPage = new ChooseRoomPage(true);
 
-                if (isAdded)
+                if (isCreated)
                 {
-                    await DisplayAlert("Dodawanie pokoju", "Pomyślnie dodano nowy pokój", "Wyjdź");
+                    await DisplayAlert("Dodawanie pokoju", "Pomyślnie dodano nowy pokój", "OK");
                 }
-                else
-                {
-                    await DisplayAlert("Dodawanie pokoju", "Niepowodzenie podczas dodawania pokoju", "Wyjdź");
-                }
+              
             }
         }
+        private void EnableView(bool state)
+        {
+            IsBusy = !state;
+            AddRoomBtn.IsEnabled = state;
+            BackBtn.IsEnabled = state;
+        }
+      
+        private async void onApiError(object o, ErrorEventArgs error)
+        {
+            await DisplayAlert("Dodawanie pokoju", error.MessageForUser, "OK");
+        }
+
     }
 }
