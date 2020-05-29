49ae449b16bfb7a0fbc0d926950854c6f5107a45
martamazurkiewicz <marta@mazurkiewi.cz>
2020-05-29 10:46:10 +0200

    Added test for ApiController.CreateAsset

--- Inwentaryzacja/UnitTests/ControllerTests/APITests/ApiControllerTests.cs
+++ Inwentaryzacja/UnitTests/ControllerTests/APITests/ApiControllerTests.cs
@@ -1,9 +1,12 @@
 ﻿using Inwentaryzacja.Controllers.Api;
+using Inwentaryzacja.Models;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Moq;
 
 namespace UnitTests.ApiTests
 {
@@ -17,10 +20,16 @@ namespace UnitTests.ApiTests
             apiController = new APIController();
         }
         [Test]
-        public async Task getAssetInfoTest()
+        public async Task GetAssetInfoTest()
         {
             AssetInfoEntity assetInfoEntity = await apiController.getAssetInfo(1);
             Assert.AreEqual(1,assetInfoEntity.room.id);
+        }
+        [Test]
+        public async Task CreateAssetTest()
+        {
+            AssetPrototype assetPrototype = new AssetPrototype(new AssetType(3,"monitor",'m'));
+            Assert.AreEqual(true, await apiController.CreateAsset(assetPrototype));
         }
     }
 }
