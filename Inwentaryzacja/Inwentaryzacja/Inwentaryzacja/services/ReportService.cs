10a3cb87eda782dba26cac13824a027d43e8d89a
Daniel <gaa848@gmail.com>
2020-05-30 15:12:27 +0200

    Przeniesienie logiki to ReportService

--- Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/services/ReportService.cs
+++ Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/services/ReportService.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Inwentaryzacja.Controllers.Api;
 using Inwentaryzacja.Models;
 
@@ -38,11 +39,9 @@ namespace Inwentaryzacja.Services
             throw new System.Exception("Not implemented");
         }
 
-        private ReportPosition[] GetReportPositions(int reportId)
+        public async Task<ReportPositionEntity[]> GetReportPositions(int reportId)
         {
-            // ReportPositionEntity[] reportPositionEntity = api.getReportPositions(reportId).Result; // Map to ReportPosition[]
-
-            throw new System.Exception("Not implemented");
+            return await api.getReportPositions(reportId);
         }
 
         public bool AddNewReport(ReportPrototype newReport)
@@ -53,6 +52,148 @@ namespace Inwentaryzacja.Services
         public bool ExportReportToPDF(int reportId)
         {
             throw new System.Exception("Not implemented");
+        }
+
+        public string[] GetScannedItemsCount(ReportPositionEntity[] reportPositionEntities, RoomEntity currentRoom)
+        {
+            string[] result = new string[10];
+
+            Dictionary<string, int> inThisRoomCount = new Dictionary<string, int>();
+            Dictionary<string, int> movedToRoomCount = new Dictionary<string, int>();
+            Dictionary<string, int> movedFromRoomCount = new Dictionary<string, int>();
+            Dictionary<string, int> inAnotherRoomCount = new Dictionary<string, int>();
+            Dictionary<string, int> scannedAll = new Dictionary<string, int>();
+
+            Dictionary<string, string> scannedAllDetails = new Dictionary<string, string>();
+            Dictionary<string, string> inThisRoomDetails = new Dictionary<string, string>();
+            Dictionary<string, string> movedToRoomDetails = new Dictionary<string, string>();
+            Dictionary<string, string> movedFromRoomDetails = new Dictionary<string, string>();
+            Dictionary<string, string> inAnotherRoomDetails = new Dictionary<string, string>();
+
+            foreach (ReportPositionEntity item in reportPositionEntities)
+            {
+                string typeName = item.asset.type.name;
+                if (item.present == true)
+                {
+                    if (!scannedAll.ContainsKey(typeName))
+                    {
+                        scannedAll.Add(typeName, 1);
+                        scannedAllDetails.Add(typeName, item.asset.id + "");
+                    }
+                    else
+                    {
+                        scannedAll[typeName]++;
+                        scannedAllDetails[typeName] += ", " + item.asset.id;
+                    }
+                }
+                if (item.present == true && item.previous_room == currentRoom)
+                {
+                    if (!inThisRoomCount.ContainsKey(typeName))
+                    {
+                        inThisRoomCount.Add(typeName, 1);
+                        inThisRoomDetails.Add(typeName, item.asset.id + "");
+                    }
+                    else
+                    {
+                        inThisRoomCount[typeName]++;
+                        inThisRoomDetails[typeName] += ", " + item.asset.id;
+                    }
+                }
+
+                else if (item.present == true && item.previous_room != currentRoom)
+                {
+                    if (!movedToRoomCount.ContainsKey(typeName))
+                    {
+                        movedToRoomCount.Add(typeName, 1);
+                        movedToRoomDetails.Add(typeName, item.asset.id + "");
+                    }
+                    else
+                    {
+                        movedToRoomCount[typeName]++;
+                        movedToRoomDetails[typeName] += ", " + item.asset.id;
+                    }
+                }
+                else if (item.present == false && item.previous_room == currentRoom)
+                {
+                    if (!movedFromRoomCount.ContainsKey(typeName))
+                    {
+                        movedFromRoomCount.Add(typeName, 1);
+                        movedFromRoomDetails.Add(typeName, item.asset.id + "");
+                    }
+                    else
+                    {
+                        movedFromRoomCount[typeName]++;
+                        movedFromRoomDetails[typeName] += ", " + item.asset.id;
+                    }
+                }
+                else if (item.present == false && item.previous_room != currentRoom)
+                {
+                    if (!inAnotherRoomCount.ContainsKey(typeName))
+                    {
+                        inAnotherRoomCount.Add(typeName, 1);
+                        inAnotherRoomDetails.Add(typeName, item.asset.id + "");
+                    }
+                    else
+                    {
+                        inAnotherRoomCount[typeName]++;
+                        inAnotherRoomDetails[typeName] += ", " + item.asset.id;
+                    }
+                }
+            }
+
+            result[0] = GenerateString(inThisRoomCount);
+            result[1] = GenerateString(movedToRoomCount);
+            result[2] = GenerateString(movedFromRoomCount);
+            result[3] = GenerateString(inAnotherRoomCount);
+            result[4] = GenerateString(scannedAll);
+
+            result[5] = GenerateString(scannedAllDetails);
+            result[6] = GenerateString(inThisRoomDetails);
+            result[7] = GenerateString(movedToRoomDetails);
+            result[8] = GenerateString(movedFromRoomDetails);
+            result[9] = GenerateString(inAnotherRoomDetails);
+
+            return result;
+        }
+        private string GenerateString(Dictionary<string, int> dict)
+        {
+            string result = "";
+
+            if (dict.Count == 0 || dict == null) return result;
+
+            foreach (KeyValuePair<string, int> item in dict)
+            {
+                string piecesText = "sztuk";
+                string spacebars = "";
+                int spaceCounter = 12 - item.Key.Length;
+
+                if (item.Key == "krzes³o") spaceCounter += 3;
+                if (item.Key == "monitor") spaceCounter += 3;
+                if (item.Key == "stó³") spaceCounter += 7;
+                if (item.Key == "tablica") spaceCounter += 4;
+                if (item.Key == "projektor") spaceCounter += 3;
+
+                for (int i = 0; i < spaceCounter; i++) spacebars += " ";
+
+                if (item.Value == 1) piecesText = "sztuka";
+                if (item.Value == 2 || item.Value == 3 || item.Value == 4) piecesText = "sztuki";
+
+                result += item.Key + spacebars + item.Value + " " + piecesText + Environment.NewLine;
+            }
+
+            return result;
+        }
+        private string GenerateString(Dictionary<string, string> dict)
+        {
+            string result = "";
+
+            if (dict.Count == 0 || dict == null) return result;
+
+            foreach (KeyValuePair<string, string> item in dict)
+            {
+                result += item.Key + " numer: " + item.Value + Environment.NewLine;
+            }
+            return result;
         }
 
 #if false // Old
--- Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/views/view-allReports/AllReportsPage.xaml.cs
+++ Inwentaryzacja/Inwentaryzacja/Inwentaryzacja/views/view-allReports/AllReportsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Inwentaryzacja.Controllers.Api;
 using Inwentaryzacja.Models;
+using Inwentaryzacja.Services;
 using Inwentaryzacja.views.view_allReports;
 using System;
 using System.Collections.Generic;
@@ -67,11 +68,11 @@ namespace Inwentaryzacja
         private async void ReportList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
             EnableView(false);
+            ReportService reportService = new ReportService(api);
 
             AllReport selectedReport = (AllReport)ReportList.SelectedItem;
 
             ReportHeaderEntity reportHeaderEntity = null;
-            ReportPositionEntity[] reportPositionEntities;
 
             if (selectedReport == null) return;
             foreach (ReportHeaderEntity item in reportHeaders)
@@ -83,13 +84,10 @@ namespace Inwentaryzacja
             }
             if (reportHeaderEntity == null) return;
 
-            Task<ReportPositionEntity[]> reportPositionTask = api.getReportPositions(reportHeaderEntity.id);
-            
-            await reportPositionTask;
-            reportPositionEntities = reportPositionTask.Result;
+            ReportPositionEntity[] reportPositionEntities = await reportService.GetReportPositions(reportHeaderEntity.id);
             if (reportPositionEntities == null) return;
 
-            string[] counted = GetScannedItemsCount(reportPositionEntities, reportHeaderEntity.room);
+            string[] counted = reportService.GetScannedItemsCount(reportPositionEntities, reportHeaderEntity.room);
 
             string inThisRoom = counted[0];
             string moveToRoom = counted[1];
@@ -114,149 +112,6 @@ namespace Inwentaryzacja
 
             EnableView(true);
             App.Current.MainPage = new ReportDetailsView(headerText, roomText, createDate, createTime, ownerText, inThisRoom, moveToRoom, moveFromRoom, inAnotherRoom, scannedAll, scannedAllDetails, inThisRoomDetails, movedToRoomDetails, movedFromRoomDetails, inAnotherRoomDetails);
-        }
-
-        private string[] GetScannedItemsCount(ReportPositionEntity[] reportPositionEntities, RoomEntity currentRoom)
-        {
-            string[] result = new string[10];
-
-            Dictionary<string, int> inThisRoomCount = new Dictionary<string, int>();
-            Dictionary<string, int> movedToRoomCount = new Dictionary<string, int>();
-            Dictionary<string, int> movedFromRoomCount = new Dictionary<string, int>();
-            Dictionary<string, int> inAnotherRoomCount = new Dictionary<string, int>();
-            Dictionary<string, int> scannedAll = new Dictionary<string, int>();
-
-            Dictionary<string, string> scannedAllDetails = new Dictionary<string, string>();
-            Dictionary<string, string> inThisRoomDetails = new Dictionary<string, string>();
-            Dictionary<string, string> movedToRoomDetails = new Dictionary<string, string>();
-            Dictionary<string, string> movedFromRoomDetails = new Dictionary<string, string>();
-            Dictionary<string, string> inAnotherRoomDetails = new Dictionary<string, string>();
-
-            foreach (ReportPositionEntity item in reportPositionEntities)
-            {
-                string typeName = item.asset.type.name;
-                if (item.present == true)
-                {
-                    if (!scannedAll.ContainsKey(typeName))
-                    {
-                        scannedAll.Add(typeName, 1);
-                        scannedAllDetails.Add(typeName, item.asset.id + "");
-                    }
-                    else
-                    {
-                        scannedAll[typeName]++;
-                        scannedAllDetails[typeName] += ", " + item.asset.id;
-                    }
-                }
-                if (item.present == true && item.previous_room == currentRoom)
-                {
-                    if (!inThisRoomCount.ContainsKey(typeName))
-                    {
-                        inThisRoomCount.Add(typeName, 1);
-                        inThisRoomDetails.Add(typeName, item.asset.id + "");
-                    }
-                    else
-                    {
-                        inThisRoomCount[typeName]++;
-                        inThisRoomDetails[typeName] += ", " + item.asset.id;
-                    }
-                }
-
-                else if (item.present == true && item.previous_room != currentRoom)
-                {
-                    if (!movedToRoomCount.ContainsKey(typeName))
-                    {
-                        movedToRoomCount.Add(typeName, 1);
-                        movedToRoomDetails.Add(typeName, item.asset.id + "");
-                    }
-                    else
-                    {
-                        movedToRoomCount[typeName]++;
-                        movedToRoomDetails[typeName] += ", " + item.asset.id;
-                    }
-                }
-                else if (item.present == false && item.previous_room == currentRoom)
-                {
-                    if (!movedFromRoomCount.ContainsKey(typeName))
-                    {
-                        movedFromRoomCount.Add(typeName, 1);
-                        movedFromRoomDetails.Add(typeName, item.asset.id + "");
-                    }
-                    else
-                    {
-                        movedFromRoomCount[typeName]++;
-                        movedFromRoomDetails[typeName] += ", " + item.asset.id;
-                    }
-                }
-                else if (item.present == false && item.previous_room != currentRoom)
-                { 
-                    if (!inAnotherRoomCount.ContainsKey(typeName))
-                    {
-                        inAnotherRoomCount.Add(typeName, 1);
-                        inAnotherRoomDetails.Add(typeName, item.asset.id + "");
-                    }
-                    else
-                    {
-                        inAnotherRoomCount[typeName]++;
-                        inAnotherRoomDetails[typeName] += ", " + item.asset.id;
-                    }
-                }
-            }
-
-            result[0] = GenerateString(inThisRoomCount);
-            result[1] = GenerateString(movedToRoomCount);
-            result[2] = GenerateString(movedFromRoomCount);
-            result[3] = GenerateString(inAnotherRoomCount);
-            result[4] = GenerateString(scannedAll);
-
-            result[5] = GenerateString(scannedAllDetails);
-            result[6] = GenerateString(inThisRoomDetails);
-            result[7] = GenerateString(movedToRoomDetails);
-            result[8] = GenerateString(movedFromRoomDetails);
-            result[9] = GenerateString(inAnotherRoomDetails);
-
-            return result;
-        }
-        
-        private string GenerateString(Dictionary<string, int> dict)
-        {
-            string result = "";
-
-            if (dict.Count == 0 || dict == null) return result;
-
-            foreach (KeyValuePair<string,int> item in dict)
-            {
-                string piecesText = "sztuk";
-                string spacebars = "";
-                int spaceCounter = 12 - item.Key.Length;
-
-                if (item.Key == "krzesło") spaceCounter += 3;
-                if (item.Key == "monitor") spaceCounter += 3;
-                if (item.Key == "stół") spaceCounter += 7;
-                if (item.Key == "tablica") spaceCounter += 4;
-                if (item.Key == "projektor") spaceCounter += 3;
-
-                for (int i = 0; i < spaceCounter; i++) spacebars += " ";                
-
-                if (item.Value == 1) piecesText = "sztuka";
-                if (item.Value == 2|| item.Value == 3|| item.Value == 4) piecesText = "sztuki";
-
-                result += item.Key + spacebars + item.Value + " " + piecesText + Environment.NewLine;
-            }
-
-            return result;
-        }
-        private string GenerateString(Dictionary<string, string> dict)
-        {
-            string result = "";
-
-            if (dict.Count == 0 || dict == null) return result;
-
-            foreach (KeyValuePair<string, string> item in dict)
-            {
-                result += item.Key + " numer: " + item.Value + Environment.NewLine;
-            }
-            return result;
         }
 
         private void EnableView(bool state)
