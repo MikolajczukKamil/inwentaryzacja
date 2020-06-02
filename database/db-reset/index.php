cc6664c73759cfd50fdc7eccd3d20b1ed3db3a2f
Kamil Mikołajczuk <k.mikolajczuk123@gmail.com>
2020-06-02 11:58:46 +0200

    getContent fn

--- database/db-reset/index.php
+++ database/db-reset/index.php
@@ -9,12 +9,23 @@
 		<div id="kontener">
 			<h1>Reset bazy danych</h1>
 			<form id="logowanie" action="./index.php" method="post">
-				<p><input type="password" id="password" name="password" placeholder="Wpisz hasło..."/></p>
-				<p><button class="przycisk" type="submit" name="akcja" value="p1">Resetuj bazę danych</button></p>
+				<p><input type="password" id="password" name="password" placeholder="Wpisz hasło..." value="<?php if(isset($_POST['password'])) echo $_POST['password']; ?>"/></p>
+				<p><button class="przycisk" type="submit" name="akcja" value="p1">Resetuj dane</button></p>
 				<p><button class="przycisk" type="submit" name="akcja" value="p2">Resetuj procedury</button></p>
-				<p><button class="przycisk" type="submit" name="akcja" value="p3">Resetuj bazę danych oraz procedury</button></p>
+				<p><button class="przycisk" type="submit" name="akcja" value="p3">Resetuj funkcje</button></p>
+				<p><button class="przycisk" type="submit" name="akcja" value="p4">Resetuj wszystko</button></p>
 			</form>
 			<?php
+				function getContent($url){
+					$ch = curl_init();
+					curl_setopt($ch, CURLOPT_URL, $url);
+					curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+					curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+					$str = curl_exec($ch);
+					curl_close($ch);
+					return $str;
+				}
+				
 				require_once './connection.php';
 
 				error_reporting(0);
@@ -22,18 +33,10 @@
 				if(isset($_POST['akcja']) && isset($_POST['password']))
 				{
 					error_reporting(0);
-					$password = "ResetBazy102938";
+					$passwords = ["ResetBazy102938", "reset"];
 					
-					if($_POST['password']===$password)
+					if(in_array($_POST['password'], $passwords))
 					{
-						$conn = getConnection();
-
-						if($conn->connect_error)
-						{
-							echo '<p class="komunikatBad">Nie udało się połączyć z bazą danych</p>';
-							die();
-						}
-						
 						$url = 'https://raw.githubusercontent.com/MikolajczukKamil/inwentaryzacja/database/database/dist/';
 						
 						if($_POST['akcja']==="p1")
@@ -46,15 +49,30 @@
 						}
 						else if($_POST['akcja']==="p3")
 						{
+							$f = 'db-functions.sql';
+						}
+						else if($_POST['akcja']==="p4")
+						{
 							$f = 'db-full.sql';
 						}
+						else {
+							echo '<p class="komunikatBad">Akcja nie znana</p>';
+							die();
+						}
 
-						$content = file_get_contents($url . $f);
-						
+						$conn = getConnection();
+
+						if($conn->connect_error)
+						{
+							echo '<p class="komunikatBad">Nie udało się połączyć z bazą danych</p>';
+							die();
+						}
+
+						$content = getContent($url . $f);
 						
 						if($content)
 						{
-							if($conn->multi_query($content) === TRUE)
+							if($conn->multi_query($content))
 							{
 								echo '<p class="komunikatGood">Wykonano reset bazy danych!</p>';
 							}
